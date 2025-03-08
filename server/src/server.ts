a0e2f93313af27a178c4fc4b89ee6d2da3da4662
musserdn <musserdn@gmail.com>
2025-03-08 12:41:51 -0600

    paths for render

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import path from 'node:path';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
