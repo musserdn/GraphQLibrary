a8f5313253db1ce5925ece45755e488513e69e54
musserdn <musserdn@gmail.com>
2025-03-15 13:04:26 -0500

    es scope for render

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,10 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
